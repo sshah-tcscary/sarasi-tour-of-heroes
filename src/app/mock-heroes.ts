ddf3f8e67615cbdafc3daf12b7b5bf4582e9e1ed
sshah-tcscary <sarasishah@gmail.com>
2020-04-25 12:55:47 -0400

    1 binaries uploaded

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,7 +5,7 @@ export const HEROES: Hero[] = [
   { id: 1.98, name: 'Pretty in Pink',  description: '???', imageUrl: 'https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image.png' },
   { id: 1.97, name: 'Ocean Shore',  description: 'Bring the icy blue waters and sunny days into your life! This new bracelet features all the beautiful colors of the sea.', imageUrl: 'https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image%20(1).png' },
   { id: 2.99, name: 'Charlie Brown',  description: 'Our favorite TV show featured in our bracelet.' },
-  { id: 1.96, name: 'Rainforest',  description: 'Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust!' },
+  { id: 1.96, name: 'Rainforest',  description: 'Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust!', imageUrl: '' },
   { id: 3.99, name: 'Seasons pack',  description: 'This set of four bracelets lets you embrace the four seasons.' },
   { id: 1.95, name: 'Coral Bay',  description: 'hello' },
   { id: 1.94, name: 'Stormy Sea',  description: 'This bracelet will help you experience the beautiful rough sea and its magnificent waves. Our new bracelet features all the colors of the deep sea!' },
