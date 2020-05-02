ed0aaab8b84a2d1e4a45c1ef41e73eb1266bdf24
sshah-tcscary <sarasishah@gmail.com>
2020-05-02 13:04:18 -0400

    2 files modified, 6 deleted

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,57 +3,65 @@ import { Hero } from "./hero";
 export const HEROES: Hero[] = [
   {
     id: 1.99,
-    name: "Sun in the Morning",
+    name: "Sun in the morning",
     description:
-      "Bring a little sunshine into your days with our new sun in the morning bracelet! This features all of the sunset colors to brighten up your day.",
+      "Bring a little sunshine into your day with our new sun in the moring bracelet! This features a sun charm with all the sunset colors to brighten up your day!",
     imageUrl:
-      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/IMG_0387%20(1).jpg"
+      "https://cdn.jsdelivr.net/gh/cweerasinha/tour-of-heros-chavi@master/src/app/assets/IMG_0387%20(1).jpg"
   },
   {
     id: 1.98,
     name: "Pretty in Pink",
-    description: "???",
+    description: "hello",
     imageUrl:
-      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image.png"
+      "https://cdn.jsdelivr.net/gh/cweerasinha/tour-of-heros-chavi@master/src/app/assets/IMG-6729%20(1).jpg"
   },
   {
     id: 1.97,
     name: "Ocean Shore",
     description:
-      "Bring the icy blue waters and sunny days into your life! This new bracelet features all the beautiful colors of the sea.",
+      "Bring the icy blue waters and sunny days into your life! THis new bracelet features all the beautiul colors of the sea and helps you vision your perfect life",
     imageUrl:
-      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image%20(1).png",
+      "https://cdn.jsdelivr.net/gh/cweerasinha/tour-of-heros-chavi@master/src/app/assets/Ocean%20shore%20bracelet.JPG",
     imageUrl2:
-       "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/IMG_6813.JPG" 
+      "https://cdn.jsdelivr.net/gh/cweerasinha/tour-of-heros-chavi@master/src/app/assets/Ocean%20shore%20bracelet%202.JPG"
+
   },
   {
-    id: 2.99,
+    id: 2.02,
     name: "Charlie Brown",
-    description: "Our favorite TV show featured in our bracelet."
+    description:
+      "from your favorite tv show, Charlie Brown is featured in our new bracelet! "
   },
   {
     id: 1.96,
     name: "Rainforest",
     description:
-      "Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust!",
-    imageUrl: ""
+      "Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust! ",
+    imageUrl:
+      "https://cdn.jsdelivr.net/gh/cweerasinha/tour-of-heros-chavi@master/src/app/assets/Rainforest.JPG"
   },
   {
     id: 3.99,
     name: "Seasons pack",
-    description: "This set of four bracelets lets you embrace the four seasons."
+    description:
+      "This set of four bracelets, lets you embrace the four seasons like never before!"
   },
-  { id: 1.95, name: "Coral Bay", description: "hello" },
+  {
+    id: 1.95,
+    name: "Coral Bay",
+    description: "All the colors of the corals bays "
+  },
   {
     id: 1.94,
     name: "Stormy Sea",
     description:
-      "This bracelet will help you experience the beautiful rough sea and its magnificent waves. Our new bracelet features all the colors of the deep sea!"
+      " This bracelet will help you experience the beautiful rough sea and its magnificent waves. Our new bracelet features all the colors of the deep sea! "
   },
   {
     id: 1.93,
     name: "Purple Wonders",
-    description: "Featuring lots of purple its sure to bring wonders to you!"
+    description: " Featuring lots of purple its sure to bring wonders to you!"
   },
-  { id: 2.98, name: "Delightful Daisies ", description: "hi" }
+  { id: 2.01, name: "Delightful Daisies", description: "hello my life" }
 ];
