736d829244923d4d5ed06e56af005cf9ddc6b63b
sshah-tcscary <sarasishah@gmail.com>
2020-05-02 12:34:11 -0400

    1 binaries uploaded

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,59 @@
-import { Hero } from './hero';
+import { Hero } from "./hero";
 
 export const HEROES: Hero[] = [
-  { id: 1.99, name: 'Sun in the Morning',   description: 'Bring a little sunshine into your days with our new sun in the morning bracelet! This features all of the sunset colors to brighten up your day.', imageUrl: 'https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/IMG_0387%20(1).jpg' },
-  { id: 1.98, name: 'Pretty in Pink',  description: '???', imageUrl: 'https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image.png' },
-  { id: 1.97, name: 'Ocean Shore',  description: 'Bring the icy blue waters and sunny days into your life! This new bracelet features all the beautiful colors of the sea.', imageUrl: 'https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image%20(1).png' },
-  { id: 2.99, name: 'Charlie Brown',  description: 'Our favorite TV show featured in our bracelet.' },
-  { id: 1.96, name: 'Rainforest',  description: 'Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust!', imageUrl: '' },
-  { id: 3.99, name: 'Seasons pack',  description: 'This set of four bracelets lets you embrace the four seasons.' },
-  { id: 1.95, name: 'Coral Bay',  description: 'hello' },
-  { id: 1.94, name: 'Stormy Sea',  description: 'This bracelet will help you experience the beautiful rough sea and its magnificent waves. Our new bracelet features all the colors of the deep sea!' },
-  { id: 1.93, name: 'Purple Wonders',  description: 'Featuring lots of purple its sure to bring wonders to you!' },
-  { id: 2.98, name: 'Delightful Daisies ',  description: 'hi' }
+  {
+    id: 1.99,
+    name: "Sun in the Morning",
+    description:
+      "Bring a little sunshine into your days with our new sun in the morning bracelet! This features all of the sunset colors to brighten up your day.",
+    imageUrl:
+      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/IMG_0387%20(1).jpg"
+  },
+  {
+    id: 1.98,
+    name: "Pretty in Pink",
+    description: "???",
+    imageUrl:
+      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image.png"
+  },
+  {
+    id: 1.97,
+    name: "Ocean Shore",
+    description:
+      "Bring the icy blue waters and sunny days into your life! This new bracelet features all the beautiful colors of the sea.",
+    imageUrl:
+      "https://cdn.jsdelivr.net/gh/sshah-tcscary/sarasi-tour-of-heroes@master/src/app/assets/image%20(1).png",
+    imageUrl:
+       "" 
+  },
+  {
+    id: 2.99,
+    name: "Charlie Brown",
+    description: "Our favorite TV show featured in our bracelet."
+  },
+  {
+    id: 1.96,
+    name: "Rainforest",
+    description:
+      "Together we can make a difference in our world. By wearing this bracelet you are helping and raising awareness for the Rainforest Trust!",
+    imageUrl: ""
+  },
+  {
+    id: 3.99,
+    name: "Seasons pack",
+    description: "This set of four bracelets lets you embrace the four seasons."
+  },
+  { id: 1.95, name: "Coral Bay", description: "hello" },
+  {
+    id: 1.94,
+    name: "Stormy Sea",
+    description:
+      "This bracelet will help you experience the beautiful rough sea and its magnificent waves. Our new bracelet features all the colors of the deep sea!"
+  },
+  {
+    id: 1.93,
+    name: "Purple Wonders",
+    description: "Featuring lots of purple its sure to bring wonders to you!"
+  },
+  { id: 2.98, name: "Delightful Daisies ", description: "hi" }
 ];
